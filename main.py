1476469e1f62484be0d6313b711d8e463c4200cb
youjaebeom <dbwo4011@gmail.com>
2021-05-15 01:41:11 +0900

    gui start

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ import pandas as pd
 from pybithumb.core import *
 from xcoin_api_client import *
 import pybithumb
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+
+
 
 def coin_ticker_public(coin_name_list):
 
@@ -103,6 +108,7 @@ def coin_candlestick(coin_name_list,chart_intervals_list,sto_N,sto_m,sto_t):
 
             print(df)
             print(df.iloc[len(df)-1]["stochastic%K"])
+            result.config(text=answer)
 
 def get_account(coin):
     api = XCoinAPI(con_key, sec_key)
@@ -166,8 +172,66 @@ def market_sell(coin):
 
     print("response", result)
 
+def gui():
+    win = tk.Tk()
+
+    win.title("Python GUI")
+
+    a_label = ttk.Label(win, text="A Label")
+    a_label.grid(column=0, row=0)
+
+    def click_me():
+        action.configure(text='Hello ' + name.get() + ' ' + number_chosen.get())
+
+    ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
+
+    name = tk.StringVar()
+    name_entered = ttk.Entry(win, width=12, textvariable=name)
+    name_entered.grid(column=0, row=1)
+
+    action = ttk.Button(win, text="Click Me!", command=click_me)
+    action.grid(column=2, row=1)  # <= change column to 2
+
+    ttk.Label(win, text="Choose a number:").grid(column=1, row=0)
+    number = tk.StringVar()
+    number_chosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+    number_chosen['values'] = (1, 2, 4, 42, 100)
+    number_chosen.grid(column=1, row=1)
+    number_chosen.current(0)
+
+    chVarDis = tk.IntVar()
+    check1 = tk.Checkbutton(win, text="Disabled", variable=chVarDis, state='disabled')
+    check1.select()
+    check1.grid(column=0, row=4, sticky=tk.W)
+
+    chVarUn = tk.IntVar()
+    check2 = tk.Checkbutton(win, text="UnChecked", variable=chVarUn)
+    check2.deselect()
+    check2.grid(column=1, row=4, sticky=tk.W)
+
+    chVarEn = tk.IntVar()
+    check3 = tk.Checkbutton(win, text="Enabled", variable=chVarEn)
+    check3.deselect()
+    check3.grid(column=2, row=4, sticky=tk.W)
+
+
+
+    # 스크롤 텍스트
+    scrol_w = 50
+    scrol_h = 3
+    scr = scrolledtext.ScrolledText(win, width=scrol_w, height=scrol_h, wrap=tk.WORD)
+    scr.grid(column=0, columnspan=3)
+
+    name_entered.focus()
+
+    win.mainloop()
+
+
+
 
 if __name__ == '__main__':
+    gui()
+
     #coin_name_list = ["BTC", "XRP", "ETH"]
     coin_name_list = ["BTC"]
     #chart_intervals_list = ['1m', '3m', '5m', '10m', '30m', '1h', '6h', '12h', '24h']
@@ -181,7 +245,7 @@ if __name__ == '__main__':
 
     #coin_ticker_public(coin_name_list)
 
-    #coin_candlestick(coin_name_list,chart_intervals_list,sto_N,sto_m,sto_t)
+    coin_candlestick(coin_name_list,chart_intervals_list,sto_N,sto_m,sto_t)
 
     global con_key
     con_key="92ccf25a931830a4c3dd664662d22011"
@@ -190,7 +254,7 @@ if __name__ == '__main__':
 
 
 
-    market_buy("ETC")
+    #market_buy("ETC")
     #market_sell("ETC")
 
 
